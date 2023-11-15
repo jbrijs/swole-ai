40a45c463b4967c6fe0a21d48e33ddb38ba8eaf4
jbrijs <joebrijs@gmail.com>
2023-11-14 18:58:38 -0700

    moved profile model to registration, deleted redundant user model

--- _server/core/models.py
+++ _server/core/models.py
@@ -1,36 +1,5 @@
 from django.db import models
 
-class User(models.Model):
-    id = models.BigAutoField(primary_key=True)
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
-    email = models.EmailField(unique=True)  # Ensures that email is unique across users
-
-class Profile(models.Model):
-    id = models.BigAutoField(primary_key=True)
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
-
-    class Sex(models.TextChoices):
-        MALE = 'M', 'Male'
-        FEMALE = 'F', 'Female'
-        UNSPECIFIED = 'U', 'Do not wish to specify/Other'
-    sex = models.CharField(max_length=1, choices=Sex.choices)
-
-    age = models.IntegerField(null=True, blank=True)
-
-    class Experience(models.TextChoices):
-        NOVICE = 'N', 'Novice'
-        BEGINNER = 'B', 'Beginner'
-        INTERMEDIATE = 'I', 'Intermediate'
-        ADVANCED = 'A', 'Advanced'
-    experience = models.CharField(max_length=1, choices=Experience.choices)
-
-    class Goals(models.TextChoices):
-        STRENGTH = 'S', 'Strength'
-        FAT_LOSS = 'F', 'Fat loss'
-        MUSCLE_GAIN = 'M', 'Muscle gain'
-    goals = models.CharField(max_length=1, choices=Goals.choices)
-
 class Plan(models.Model):
     id = models.BigAutoField(primary_key=True)
     name = models.CharField(max_length=100)
