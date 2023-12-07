0e3030e8c70ecc597bae9bf1ad92a00dd0b572ce
jbrijs <joebrijs@gmail.com>
2023-12-06 19:08:03 -0700

    moved all models except for user to the api app

--- _server/api/models.py
+++ _server/api/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from ..core.models import Profile
 
-# Create your models here.
+class Plan(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    profile = models.OneToOneField(Profile, on_delete=models.CASCADE, related_name='plan')
+
+
+class Week(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    plan = models.ForeignKey(Plan, on_delete=models.CASCADE, related_name='weeks')
+
+class Day(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    week = models.ForeignKey(Week, on_delete=models.CASCADE, related_name='days')
+
+class Exercise(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    sets = models.IntegerField()
+    reps = models.IntegerField()
+    day = models.ForeignKey(Day, on_delete=models.CASCADE, related_name='exercises')
+
--- _server/core/models.py
+++ _server/core/models.py
@@ -27,22 +27,3 @@ class Profile(models.Model):
     goal = models.CharField(max_length=1, choices=Goal.choices)
 
 
-class Plan(models.Model):
-    id = models.BigAutoField(primary_key=True)
-    name = models.CharField(max_length=100)
-
-class Week(models.Model):
-    id = models.BigAutoField(primary_key=True)
-    plan = models.ForeignKey(Plan, on_delete=models.CASCADE, related_name='weeks')
-
-class Day(models.Model):
-    id = models.BigAutoField(primary_key=True)
-    name = models.CharField(max_length=50)
-    week = models.ForeignKey(Week, on_delete=models.CASCADE, related_name='days')
-
-class Exercise(models.Model):
-    id = models.BigAutoField(primary_key=True)
-    name = models.CharField(max_length=100)
-    sets = models.IntegerField()
-    reps = models.IntegerField()
-    day = models.ForeignKey(Day, on_delete=models.CASCADE, related_name='exercises')
