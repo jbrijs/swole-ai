4625082456679278f8a444e996a983e8351f743e
jbrijs <joebrijs@gmail.com>
2023-12-07 21:17:10 -0700

    can move between weeks with buttons

--- client/src/NewPlan.jsx
+++ client/src/NewPlan.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { useLocation } from "react-router-dom";
 import ExerciseInput from "./ExerciseInput";
+import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
+import KeyboardArrowLeftIcon from "@mui/icons-material/KeyboardArrowLeft";
 
 const initializePlan = (numWeeks, daysPerWeek, exercisesPerDay) => {
   return Array.from({ length: numWeeks }, () => ({
@@ -27,6 +29,9 @@ function NewPlan() {
     e.preventDefault();
   };
 
+  console.log(numWeeks)
+  console.log(currentWeek)
+
   const handleExerciseChange = (weekIndex, dayIndex, exerciseIndex, event) => {
     const newPlan = [...plan];
     const { name, value } = event.target;
@@ -36,58 +41,82 @@ function NewPlan() {
 
   return (
     <>
-      <form
-        className="flex flex-col justify-center items-center mt-10"
-        onSubmit={handleSubmit}
-      >
-        <div key={currentWeek}>
-          <h1
-            className="text-5xl font-light text-white mb-10"
-            key={currentWeek}
-          >
-            Week {currentWeek + 1}
-          </h1>
-          {plan[currentWeek].days.map((day, dayIndex) => (
-            <div key={dayIndex}>
-              <h1
-                className="text-2xl font-light text-tertiary pb-2 pt-6"
-                key={dayIndex}
-              >
-                Day {dayIndex + 1}
-              </h1>
-              <div className="flex flex-row items-center">
-                <p className="w-1/4 text-center text-xl text-gray-700 font-semibold bg-secondary">
-                  Exercise
-                </p>
-                <p className="w-1/4 text-center text-xl text-gray-700 font-semibold bg-secondary">
-                  Sets
-                </p>
-                <p className="w-1/4 text-center text-xl text-gray-700 font-semibold bg-secondary">
-                  Reps
-                </p>
-                <p className="w-1/4 text-center text-xl text-gray-700 font-semibold bg-secondary">
-                  Weight
-                </p>
+      <div className="flex flex-col justify-center items-center">
+        <form
+          className="flex flex-col justify-center items-center mt-10"
+          onSubmit={handleSubmit}
+        >
+          <div key={currentWeek}>
+            <h1
+              className="text-5xl font-light text-white mb-10"
+              key={currentWeek}
+            >
+              Week {currentWeek + 1}
+            </h1>
+            {plan[currentWeek].days.map((day, dayIndex) => (
+              <div key={dayIndex}>
+                <h1
+                  className="text-2xl font-light text-tertiary pb-2 pt-6"
+                  key={dayIndex}
+                >
+                  Day {dayIndex + 1}
+                </h1>
+                <div className="flex flex-row items-center">
+                  <p className="w-1/4 text-center text-xl text-gray-700 font-semibold bg-secondary">
+                    Exercise
+                  </p>
+                  <p className="w-1/4 text-center text-xl text-gray-700 font-semibold bg-secondary">
+                    Sets
+                  </p>
+                  <p className="w-1/4 text-center text-xl text-gray-700 font-semibold bg-secondary">
+                    Reps
+                  </p>
+                  <p className="w-1/4 text-center text-xl text-gray-700 font-semibold bg-secondary">
+                    Weight
+                  </p>
+                </div>
+                {day.exercises.map((exercise, exerciseIndex) => (
+                  <ExerciseInput
+                    key={exerciseIndex}
+                    exercise={exercise}
+                    onChange={(e) =>
+                      handleExerciseChange(
+                        currentWeek,
+                        dayIndex,
+                        exerciseIndex,
+                        e
+                      )
+                    }
+                  />
+                ))}
               </div>
-              {day.exercises.map((exercise, exerciseIndex) => (
-                <ExerciseInput
-                  key={exerciseIndex}
-                  exercise={exercise}
-                  onChange={(e) =>
-                    handleExerciseChange(
-                      currentWeek,
-                      dayIndex,
-                      exerciseIndex,
-                      e
-                    )
-                  }
-                />
-              ))}
-            </div>
-          ))}
+            ))}
+          </div>
+          {currentWeek + 1 == numWeeks && (
+            <button type="submit">Submit Plan</button>
+          )}
+        </form>
+        <div className="relative h-20 w-1/2 mt-10">
+          {currentWeek > 0 && (
+            <button
+              onClick={() => setCurrentWeek(currentWeek - 1)}
+              className="flex flex-row items-center justify-center absolute left-0 bg-primary px-3 h-12 rounded-lg text-xl text-black hover:text-white hover:bg-secondary transition"
+            >
+              <KeyboardArrowLeftIcon />
+              Week {currentWeek}
+            </button>
+          )}
+          {currentWeek < numWeeks - 1 && (
+            <button
+              onClick={() => setCurrentWeek(currentWeek + 1)}
+              className="flex flex-row items-center justify-center absolute right-0 bg-primary px-3 h-12 rounded-lg text-xl text-black hover:text-white hover:bg-secondary transition"
+            >
+              Week {currentWeek + 2}
+              <KeyboardArrowRightIcon />
+            </button>
+          )}
         </div>
-        <button type="submit">Submit Plan</button>
-      </form>
+      </div>
     </>
   );
 }
