146b6602bb7e63e85a4d8bffe8aab0a6d9d87cb7
jbrijs <joebrijs@gmail.com>
2023-12-07 20:39:25 -0700

    only render one week to the screen at a time now

--- client/src/NewPlan.jsx
+++ client/src/NewPlan.jsx
@@ -21,6 +21,7 @@ function NewPlan() {
   const [plan, setPlan] = useState(
     initializePlan(numWeeks, daysPerWeek, exercisesPerDay)
   );
+  const [currentWeek, setCurrentWeek] = useState(0);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -34,34 +35,60 @@ function NewPlan() {
   };
 
   return (
-    <form className="flex flex-col justify-center items-center mt-10"onSubmit={handleSubmit}>
-      {plan.map((week, weekIndex) => (
-        <div key={weekIndex}>
-          <h1 className="text-5xl font-light text-white mb-10" key={weekIndex}>Week {weekIndex + 1}</h1>
-          {week.days.map((day, dayIndex) => (
+    <>
+      <form
+        className="flex flex-col justify-center items-center mt-10"
+        onSubmit={handleSubmit}
+      >
+        <div key={currentWeek}>
+          <h1
+            className="text-5xl font-light text-white mb-10"
+            key={currentWeek}
+          >
+            Week {currentWeek + 1}
+          </h1>
+          {plan[currentWeek].days.map((day, dayIndex) => (
             <div key={dayIndex}>
-              <h1 className="text-2xl font-light text-tertiary pb-4" key={dayIndex}>Day {dayIndex + 1}</h1>
+              <h1
+                className="text-2xl font-light text-tertiary pb-2 pt-6"
+                key={dayIndex}
+              >
+                Day {dayIndex + 1}
+              </h1>
               <div className="flex flex-row items-center">
-                <p className="w-1/4 text-center text-xl text-gray-700 font-semibold bg-secondary">Exercise</p>
-                <p className="w-1/4 text-center text-xl text-gray-700 font-semibold bg-secondary">Sets</p>
-                <p className="w-1/4 text-center text-xl text-gray-700 font-semibold bg-secondary">Reps</p>
-                <p className="w-1/4 text-center text-xl text-gray-700 font-semibold bg-secondary">Weight</p>
+                <p className="w-1/4 text-center text-xl text-gray-700 font-semibold bg-secondary">
+                  Exercise
+                </p>
+                <p className="w-1/4 text-center text-xl text-gray-700 font-semibold bg-secondary">
+                  Sets
+                </p>
+                <p className="w-1/4 text-center text-xl text-gray-700 font-semibold bg-secondary">
+                  Reps
+                </p>
+                <p className="w-1/4 text-center text-xl text-gray-700 font-semibold bg-secondary">
+                  Weight
+                </p>
               </div>
               {day.exercises.map((exercise, exerciseIndex) => (
                 <ExerciseInput
                   key={exerciseIndex}
                   exercise={exercise}
                   onChange={(e) =>
-                    handleExerciseChange(weekIndex, dayIndex, exerciseIndex, e)
+                    handleExerciseChange(
+                      currentWeek,
+                      dayIndex,
+                      exerciseIndex,
+                      e
+                    )
                   }
                 />
               ))}
             </div>
           ))}
         </div>
-      ))}
-      <button type="submit">Submit Plan</button>
-    </form>
+        <button type="submit">Submit Plan</button>
+      </form>
+    </>
   );
 }
 
