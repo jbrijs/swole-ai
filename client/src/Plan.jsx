4fd53d0f0ca708b715a4a6a8a94d031be2219727
jbrijs <joebrijs@gmail.com>
2023-12-13 15:44:30 -0700

    styled plan on home page

--- client/src/Plan.jsx
+++ client/src/Plan.jsx
@@ -11,28 +11,28 @@ function Plan({ userPlan, editable, handleExerciseChange, currentWeek }) {
     <>
       <div className="flex flex-col justify-center items-center mt-10 w-full">
         <div key={currentWeek} className="w-full">
-          <h1 className="text-3xl font-light text-white mb-4" key={currentWeek}>
+          <h1 className="text-3xl font-light text-text mb-4" key={currentWeek}>
             Week {currentWeek + 1}
           </h1>
           {userPlan.weeks[currentWeek].days.map((day, dayIndex) => (
             <div key={dayIndex} className="w-full">
               <h1
-                className="text-2xl font-light text-tertiary pb-2"
+                className="text-xl text-text pb-2"
                 key={dayIndex}
               >
                 Day {dayIndex + 1}
               </h1>
               <div className="flex flex-row items-center">
-                <p className="w-1/4 text-center text-xl text-gray-700 font-semibold bg-secondary">
+                <p className="w-1/4 text-center text-xl text-white font-semibold bg-secondary">
                   Exercise
                 </p>
-                <p className="w-1/4 text-center text-xl text-gray-700 font-semibold bg-secondary">
+                <p className="w-1/4 text-center text-xl text-white font-semibold bg-secondary">
                   Sets
                 </p>
-                <p className="w-1/4 text-center text-xl text-gray-700 font-semibold bg-secondary">
+                <p className="w-1/4 text-center text-xl text-white font-semibold bg-secondary">
                   Reps
                 </p>
-                <p className="w-1/4 text-center text-xl text-gray-700 font-semibold bg-secondary">
+                <p className="w-1/4 text-center text-xl text-white font-semibold bg-secondary">
                   Weight
                 </p>
               </div>
