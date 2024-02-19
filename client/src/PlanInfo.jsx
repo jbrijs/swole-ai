282bcb14b190ac07e7d0541bb78e5234b1f14e77
jbrijs <joebrijs@gmail.com>
2024-02-19 14:07:37 -0700

    plan info page and componenets are now responsive to screen size

--- client/src/PlanInfo.jsx
+++ client/src/PlanInfo.jsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import NumberForm from './NumberForm'; // Make sure this path is correct
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import NumberForm from "./NumberForm"; // Make sure this path is correct
 
 function PlanInfo() {
   const [numWeeks, setNumWeeks] = useState(1);
@@ -10,15 +10,15 @@ function PlanInfo() {
 
   const goToNewPlan = (event) => {
     event.preventDefault();
-    navigate('/create_new_plan', { state: { numWeeks, daysPerWeek, exercisesPerDay } });
+    navigate("/create_new_plan", {
+      state: { numWeeks, daysPerWeek, exercisesPerDay },
+    });
   };
 
   return (
-    <div className="flex flex-col justify-center items-center m-20">
-      <form onSubmit={goToNewPlan} className="flex flex-col items-start w-1/2">
-        <h2 className="text-5xl mb-10 font-light text-text">
-          Plan Information
-        </h2>
+    <div className="flex flex-col justify-center items-center mx-10 sm:mx-0 sm:my-20">
+      <h2 className="text-4xl mt-10 mb-20 sm:my-10 font-light text-text">Plan Information</h2>
+      <form onSubmit={goToNewPlan} className="flex flex-col gap-2 sm:gap-0 sm:items-start w-full sm:w-1/2">
         <NumberForm
           value={numWeeks}
           label="Number of weeks"
@@ -45,7 +45,7 @@ function PlanInfo() {
         />
         <button
           type="submit"
-          className="my-4 h-12 bg-button rounded-xl px-8 mt-8 text-white shadow-md hover:bg-button_hover transition ease-in duration-150 shadow-xl"
+          className="w-full mt-40 sm:my-4 sm:w-auto h-12 bg-button rounded-xl px-8 mt-8 text-white shadow-md hover:bg-button_hover transition ease-in duration-150 shadow-xl"
         >
           Submit
         </button>
